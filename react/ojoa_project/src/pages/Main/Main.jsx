99322b557d7fe86cda06e51a4b13462b41f169ea
MEMOIRE-X78\leejk <doubt2k@gmail.com>
2023-08-10 13:44:23 +0900

    2차 프로젝트 내 할당 모든 업로드

---
+++ react/ojoa_project/src/pages/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../pages/Main/Main.css';
+import SlideImg from '../../pages/Main/SlideImg';
+
+const Main = () => {
+    return (
+        <div className="Main">
+            <SlideImg />
+        </div>
+    );
+};
+
+export default Main;
