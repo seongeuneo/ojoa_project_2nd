8f7e6e29e1bd7eb0e86d5b8ee8595db15f2c618d
어성은 <djandcl@naver.com>
2023-08-16 16:04:30 +0900

    수정

---
+++ react/ojoa_project/src/pages/ProductDetail/DetailInfo01.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./ProductDetail.css";
+
+function DetailInfo01() {
+    return (
+        <div className="DetailInfo01">
+            {/* <!-- main product detail --> */}
+            <div class="container">
+                {/* <!-- product detail dt 로 요약 --> */}
+
+                {/* <!-- 상품상세정보 --> */}
+
+                <div class="pd_part1">
+                    <img src="/images/pl_long_img.jpg" alt="" />
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default DetailInfo01;
