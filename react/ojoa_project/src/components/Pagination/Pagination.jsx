8632e5313469c3870b0de159d9e521f5f6fd79a9
MEMOIRE-X78\leejk <doubt2k@gmail.com>
2023-09-26 14:23:50 +0900

    재정비

--- react/ojoa_project/src/components/Pagination/Pagination.jsx
+++ react/ojoa_project/src/components/Pagination/Pagination.jsx
@@ -1,64 +1,29 @@
 import React from "react";
 import "./Pagination.css";
 
-function Pagination({ itemsPerPage, totalItems, currentPage, onPageChange }) {
-  const totalPages = Math.ceil(totalItems / itemsPerPage);
-  const pageNumbers = [];
-
-  for (let i = 1; i <= totalPages; i++) {
-    pageNumbers.push(i);
-  }
-
+function Pagination() {
   return (
     <div className="Pagination">
+      {/* <!-- 페이지네이션 --> */}
       <div className="ec-base-paginate">
         <ol>
-          <li>
-            <a href="#" className="first" onClick={() => onPageChange(1)}>
-              &lt;&lt;
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              onClick={() => onPageChange(currentPage > 1 ? currentPage - 1 : 1)}
-            >
-              &lt;
-            </a>
-          </li>
+          <li><a href="/.html?page=1" className="first">&lt;&lt;</a></li>
+          <li><a href="/.html?page=1">&lt;</a></li>
+          {/* <a href="/.html?page=1" className="first"><img src="../../../images/btn_page_first.gif" alt="첫 페이지" /></a>
+                <a href="/.html?page=1"><img src="../../../images/btn_page_prev.gif" alt="이전 페이지" /></a> */}
         </ol>
         <ol>
-          {pageNumbers.map((number) => (
-            <li
-              key={number}
-              className={currentPage === number ? "record" : null}
-            >
-              <a href="#" onClick={() => onPageChange(number)}>
-                {number}
-              </a>
-            </li>
-          ))}
+          <li className="record"><a href="" className="this">1</a></li>
         </ol>
         <ol>
-          <li>
-            <a
-              href="#"
-              onClick={() =>
-                onPageChange(currentPage < totalPages ? currentPage + 1 : totalPages)
-              }
-            >
-              &gt;
-            </a>
-          </li>
-          <li>
-            <a href="#" className="last" onClick={() => onPageChange(totalPages)}>
-              &gt;&gt;
-            </a>
-          </li>
+          <li><a href="/.html?page=1">&gt;</a></li>
+          <li><a href="/.html?page=1" className="last">&gt;&gt;</a></li>
         </ol>
+        {/* <a href="/.html?page=1"><img src="../../../images/btn_page_next.gif" alt="다음 페이지" /></a>
+                <a href="/.html?page=1" className="last"><img src="../../../images/btn_page_last.gif" alt="마지막 페이지" /></a> */}
       </div>
     </div>
-  );
-}
+  )
+};
 
 export default Pagination;
